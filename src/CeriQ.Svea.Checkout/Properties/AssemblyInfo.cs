b474678e6b7f383096d6d1bd38c865adedd4eacf
Eric Johansson <eric@ceriq.se>
2018-01-20 16:51:44 +0100

    Updating AssemblyInfo.

--- src/CeriQ.Svea.Checkout/Properties/AssemblyInfo.cs
+++ src/CeriQ.Svea.Checkout/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CeriQ.Svea.Checkout")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library to simplify connecting to Sveas Checkout API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CeriQ AB")]
 [assembly: AssemblyProduct("CeriQ.Svea.Checkout")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
